44e360e2bb9fb0d2c90296914460da6e641fd866
Reeboy <rbmiester@gmail.com>
2024-04-29 14:58:47 +0800

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,45 @@
+<main class="bg-white mx-auto">
+    <nav class="mx-auto container flex text-white  font-bold">
+        <div>
+            <img src="https://www.logomaker.com/api/main/images/1j+ojlxEOMkX9Wyqfhe43D6kh...GEpBVNmx3JwXs1M3EMoAJtlyEphvtu8Po5PExevg9C3ktKMcs8" alt="logo">
+        </div>
+        <ul class="flex flex-row space-x-10 ml-auto font-serif items-center ">
+            <li class="hover:bg-black hover:text-white hover:underline p-3 text-black text-3xl">Home</li>
+            <li class="hover:bg-black hover:text-white hover:underline p-3 text-black text-3xl">About</li>
+            <li class="hover:bg-black hover:text-white hover:underline p-3 text-black text-3xl">Portfolio</li>
+            <li class="hover:bg-black hover:text-white hover:underline p-3 text-black text-3xl">Contacts</li>
+        </ul> 
+    </nav>
+    <hr class="border-2 border-black">'
+    <section class="flex p-10 justify-center items-center">
+        <div>
+            <div class="text-4xl font-serif mr-20 font-bold">Designer</div>
+            <div class="text-lg mr-20"><br/>User Interface Designer with a passion<br/> for designing beautiful and functional<br/> user experiences. Minimalist who<br/> believes that less is more</div>
+        </div>
+        <img src="https://i.pinimg.com/564x/56/53/54/565354e5f2e556ed191d6517b4075413.jpg" alt="home logo">
+        <div>
+            <div class="text-4xl font-bold font-serif ml-20">[Coder]</div>
+            <div class="text-lg ml-20"><br/>Front End Developer  who focuses on<br/>writing clean,elegant and efficient<br/>code, Love HTML,CSS,WordPress<br/>and a touch of jQuery </div>
+        </div>
+    </section>
+    <div>
+        <div class="grid grid-cols-4 space-x-4 container mx-auto">
+            <div>
+                <img src="https://t3.ftcdn.net/jpg/02/11/95/04/240_F_211950412_wsPP8grGn9fbJjSsp8WOs8pYSaariDSU.jpg" alt="Box1" class="h-[350px] w-[400px] ">
+            </div>
+            <div>
+                <img src="https://t3.ftcdn.net/jpg/02/01/05/22/240_F_201052212_M2wpoRcv9qpysoPtHB8ruwnRekrVkHEx.jpg" alt="Box2" class="h-[350px]  w-[400px]">
+            </div>
+            <div>
+                <img src="https://t3.ftcdn.net/jpg/02/06/29/24/240_F_206292429_R7I0NOzkyd0hRaPOwsgji3y8eknIulRC.jpg" alt="Box3" class="h-[350px]  w-[400px]">
+            </div>
+            <div>
+                <img src="https://t4.ftcdn.net/jpg/00/97/90/99/240_F_97909912_4z43DKgO6XwTHEcL9ch6FgJU6VwnEOy9.jpg" alt="Box4" class="h-[350px]  w-[400px]">
+            </div>
+        </div>    
+    </div>    
+    <div class="mx-auto container bg-white">
+        <div class="text-lg font-mono mt-20">MY NAME IS <b>REEBOY BASCO</b> 
+        <div class="font-bold text-5xl ml-10">International Tambay with D3gree</div>
+    </div>
+</main>
